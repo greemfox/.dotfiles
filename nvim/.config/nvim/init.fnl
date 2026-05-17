32550c86099c4c27e34bc0189465090fea40d040
greemfox <pavelkurchanov.lpk@gmail.com>
2026-05-17 21:55:28 +0700

    add stuff

--- nvim/.config/nvim/init.fnl
+++ nvim/.config/nvim/init.fnl
@@ -1,3 +1,4 @@
+;; Options
 (set vim.g.mapleader ",")
 (set vim.g.maplocalleader ".")
 
@@ -5,18 +6,40 @@
 (set vim.o.number true)
 (set vim.o.relativenumber true)
 
+;; Keybinds
+(vim.keymap.set "n" "<leader>r" "<CMD>restart<CR>" {:desc "Restart nvim"})
+(vim.keymap.set "n"
+		"<leader>v"
+		(λ []
+		   (vim.pack.del (: (: (: (vim.iter (vim.pack.get))
+				          :filter
+				          (λ [x] (not x.active)))
+				       :map
+				       (λ [x] x.spec.name))
+				    :totable)))
+		{:desc "Vacuum the plugin space"})
+
+;; Plugins
 (fn gh [repo]
   (.. "https://github.com/" repo))
 
+(vim.pack.del ["paredit" "treesitter"])
+
 (vim.pack.add [(gh "Olical/nfnl")
 	       (gh "Olical/conjure")
+	       {:src (gh "julienvincent/nvim-paredit")
+		:name "paredit"}
 	       {:src (gh "nvim-treesitter/nvim-treesitter")
+	        :name "treesitter"
 	        :branch "main"
 	        :dependencies ["neovim-treesitter/treesitter-parser-registry"]
+		:ensure_installed ["fennel" "scheme"]
 		:build ":TSUpdate"}
 	       (gh "folke/which-key.nvim")])
+((. (require "nvim-paredit") :setup))
 
 (set vim.g.conjure#client#scheme#stdio#command "petite")
 (set vim.g.conjure#client#scheme#stdio#prompt_pattern "> $?")
 
 {}
+
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,10 +4,31 @@ vim.g.maplocalleader = "."
 vim.o.scrolloff = 23
 vim.o.number = true
 vim.o.relativenumber = true
+vim.keymap.set("n", "<leader>r", "<CMD>restart<CR>", {desc = "Restart nvim"})
+local function _1_()
+  local function _2_(x)
+    if (nil == x) then
+      _G.error("Missing argument x on init.fnl:16", 2)
+    else
+    end
+    return not x.active
+  end
+  local function _4_(x)
+    if (nil == x) then
+      _G.error("Missing argument x on init.fnl:18", 2)
+    else
+    end
+    return x.spec.name
+  end
+  return vim.pack.del(vim.iter(vim.pack.get()):filter(_2_):map(_4_):totable())
+end
+vim.keymap.set("n", "<leader>v", _1_, {desc = "Vacuum the plugin space"})
 local function gh(repo)
   return ("https://github.com/" .. repo)
 end
-vim.pack.add({gh("Olical/nfnl"), gh("Olical/conjure"), {src = gh("nvim-treesitter/nvim-treesitter"), branch = "main", dependencies = {"neovim-treesitter/treesitter-parser-registry"}, build = ":TSUpdate"}, gh("folke/which-key.nvim")})
+vim.pack.del({"paredit", "treesitter"})
+vim.pack.add({gh("Olical/nfnl"), gh("Olical/conjure"), {src = gh("julienvincent/nvim-paredit"), name = "paredit"}, {src = gh("nvim-treesitter/nvim-treesitter"), name = "treesitter", branch = "main", dependencies = {"neovim-treesitter/treesitter-parser-registry"}, ensure_installed = {"fennel", "scheme"}, build = ":TSUpdate"}, gh("folke/which-key.nvim")})
+require("nvim-paredit").setup()
 vim.g["conjure#client#scheme#stdio#command"] = "petite"
 vim.g["conjure#client#scheme#stdio#prompt_pattern"] = "> $?"
 return {}
