9e6ef046ee275857e6be7c003c7b8009a682feba
greemfox <pavelkurchanov.lpk@gmail.com>
2026-05-18 20:26:43 +0700

    uncramp the dot operator

--- nvim/.config/nvim/init.fnl
+++ nvim/.config/nvim/init.fnl
@@ -1,56 +1,67 @@
 ;; Options
-(set vim.g.mapleader ",")
-(set vim.g.maplocalleader ".")
+(set vim.g.mapleader " ")
+(set vim.g.maplocalleader ",")
 
-(set vim.o.scrolloff 23) ; Hail Eris
 (set vim.o.number true)
 (set vim.o.relativenumber true)
 (set vim.o.breakindent true)
 (set vim.o.ignorecase true)
+(set vim.o.scrolloff 23) ; Hail Eris
+
+(vim.cmd.colorscheme "catppuccin")
 
 ;; Keybinds
 (vim.keymap.set "n" "<leader>r" "<CMD>restart<CR>" {:desc "Restart nvim"})
 (vim.keymap.set "n"
-    "<leader>v"
-    (λ []
-       (vim.pack.del (: (: (: (vim.iter (vim.pack.get))
-                            :filter
-                            (λ [x] (not x.active)))
-                         :map
-                         (λ [x] x.spec.name))
-                      :totable)))
-    {:desc "Vacuum the plugin space"})
+                "<leader>v"
+                (λ []
+                  (vim.pack.del (: (: (: (vim.iter (vim.pack.get))
+                                         :filter
+                                         (λ [x] (not x.active)))
+                                      :map
+                                      (λ [x] x.spec.name))
+                                   :totable)))
+                {:desc "Vacuum plugins"})
+(vim.keymap.set "n"
+                "<leader>u"
+                (λ []
+                  (vim.pack.update))
+                {:desc "Update plugins"})
 
 ;; Plugins
 (λ gh [repo]
    (.. "https://github.com/" repo))
 
 (λ setup [plugin]
-   ((. (require plugin) :setup)))
+   (. (require plugin) :setup))
 
 (λ hooks [ev]
-   (let [name ev.data.spec.name kind ev.data.kind upd? (or (= kind "install") (= kind "update"))] 
+   (let [name ev.data.spec.name
+         kind ev.data.kind
+         upd? (or (= kind "install")
+                  (= kind "update"))] 
      (if (and (= name "parinfer") upd?)
-         (vim.system ["cargo" "build" "--release"] {:cwd ev.data.path})
-         (and (= name "treesitter") upd?)
-         (vim.cmd "TSUpdate"))))
+         (vim.system ["cargo" "build" "--release"] {:cwd ev.data.path}))))
 
 (vim.api.nvim_create_autocmd "PackChanged" {:callback hooks})
 
 (vim.pack.add [(gh "Olical/nfnl")
                (gh "Olical/conjure")
-               (gh "nvim-mini/mini.jump2d")
-               (gh "nvim-mini/mini.surround")
                {:src (gh "eraserhd/parinfer-rust")
                 :name "parinfer"
                 :version "master"}
-               {:src (gh "nvim-treesitter/nvim-treesitter")
-                :name "treesitter"
-                :version "main"}
-               (gh "folke/which-key.nvim")])
+               {:src (gh "romus204/tree-sitter-manager.nvim")
+                :name "treesitter"}
+               {:src (gh "nvim-mini/mini.surround")
+                :name "surround"}
+               {:src (gh "nvim-mini/mini.jump2d")
+                :name "jump2d"}
+               {:src (gh "folke/which-key.nvim")
+                :name "whichkey"}])
 
-(setup "mini.surround")
-(setup "mini.jump2d")
+((setup "tree-sitter-manager"))
+((setup "mini.surround"))
+((setup "mini.jump2d"))
 
 (set vim.g.conjure#client#scheme#stdio#command "petite")
 (set vim.g.conjure#client#scheme#stdio#prompt_pattern "> $?")
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,47 +1,52 @@
 -- [nfnl] init.fnl
-vim.g.mapleader = ","
-vim.g.maplocalleader = "."
-vim.o.scrolloff = 23
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.breakindent = true
 vim.o.ignorecase = true
+vim.o.scrolloff = 23
+vim.cmd.colorscheme("catppuccin")
 vim.keymap.set("n", "<leader>r", "<CMD>restart<CR>", {desc = "Restart nvim"})
 local function _1_()
   local function _2_(x)
     if (nil == x) then
-      _G.error("Missing argument x on init.fnl:18", 2)
+      _G.error("Missing argument x on init.fnl:20", 2)
     else
     end
     return not x.active
   end
   local function _4_(x)
     if (nil == x) then
-      _G.error("Missing argument x on init.fnl:20", 2)
+      _G.error("Missing argument x on init.fnl:22", 2)
     else
     end
     return x.spec.name
   end
   return vim.pack.del(vim.iter(vim.pack.get()):filter(_2_):map(_4_):totable())
 end
-vim.keymap.set("n", "<leader>v", _1_, {desc = "Vacuum the plugin space"})
+vim.keymap.set("n", "<leader>v", _1_, {desc = "Vacuum plugins"})
+local function _6_()
+  return vim.pack.update()
+end
+vim.keymap.set("n", "<leader>u", _6_, {desc = "Update plugins"})
 local function gh(repo)
   if (nil == repo) then
-    _G.error("Missing argument repo on init.fnl:25", 2)
+    _G.error("Missing argument repo on init.fnl:32", 2)
   else
   end
   return ("https://github.com/" .. repo)
 end
 local function setup(plugin)
   if (nil == plugin) then
-    _G.error("Missing argument plugin on init.fnl:28", 2)
+    _G.error("Missing argument plugin on init.fnl:35", 2)
   else
   end
-  return require(plugin).setup()
+  return require(plugin).setup
 end
 local function hooks(ev)
   if (nil == ev) then
-    _G.error("Missing argument ev on init.fnl:31", 2)
+    _G.error("Missing argument ev on init.fnl:38", 2)
   else
   end
   local name = ev.data.spec.name
@@ -49,16 +54,15 @@ local function hooks(ev)
   local upd_3f = ((kind == "install") or (kind == "update"))
   if ((name == "parinfer") and upd_3f) then
     return vim.system({"cargo", "build", "--release"}, {cwd = ev.data.path})
-  elseif ((name == "treesitter") and upd_3f) then
-    return vim.cmd("TSUpdate")
   else
     return nil
   end
 end
 vim.api.nvim_create_autocmd("PackChanged", {callback = hooks})
-vim.pack.add({gh("Olical/nfnl"), gh("Olical/conjure"), gh("nvim-mini/mini.jump2d"), gh("nvim-mini/mini.surround"), {src = gh("eraserhd/parinfer-rust"), name = "parinfer", version = "master"}, {src = gh("nvim-treesitter/nvim-treesitter"), name = "treesitter", version = "main"}, gh("folke/which-key.nvim")})
-setup("mini.surround")
-setup("mini.jump2d")
+vim.pack.add({gh("Olical/nfnl"), gh("Olical/conjure"), {src = gh("eraserhd/parinfer-rust"), name = "parinfer", version = "master"}, {src = gh("romus204/tree-sitter-manager.nvim"), name = "treesitter"}, {src = gh("nvim-mini/mini.surround"), name = "surround"}, {src = gh("nvim-mini/mini.jump2d"), name = "jump2d"}, {src = gh("folke/which-key.nvim"), name = "whichkey"}})
+setup("tree-sitter-manager")()
+setup("mini.surround")()
+setup("mini.jump2d")()
 vim.g["conjure#client#scheme#stdio#command"] = "petite"
 vim.g["conjure#client#scheme#stdio#prompt_pattern"] = "> $?"
 return {}
